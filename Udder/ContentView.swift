e87e8e8d3509d5e460c8d72cf90c4ae72f4d9df0
Hoai <dotrunghoai@gmail.com>
2021-12-12 01:23:23 +0700

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Đỗ Trung Hoài on 12/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
